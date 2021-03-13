58f29ff50d3da51e06aa22f666b163cfff9265e2
Mohamed Essam <mohamedessam.moe@gmail.com>
2021-03-13 22:45:40 +0200

    modified middleware

--- middleware.js
+++ middleware.js
@@ -2,24 +2,44 @@ const connection = require('./config');
 
 exports.getAllitems = (req, res) => {
     const limit = 25
-    const page = req.query.page
-    const offset = (page - 1) * limit
-    const table = req.params.table
-    const prodsQuery = "select * from " + table + " limit " + limit + " OFFSET " + offset
-    connection.query(prodsQuery, function (error, results, fields) {
-        if (error) throw error;
-        var jsonResult = {
-            'items_page_count': results.length,
-            'page_number': page,
-            'Items': results
-        }
+    if (req.query.page) {
+        const page = req.query.page
+        const offset = (page - 1) * limit
+        const table = req.params.table
+        const prodsQuery = "select * from " + table + " limit " + limit + " OFFSET " + offset
+        connection.query(prodsQuery, function (error, results, fields) {
+            if (error) throw error;
+            var jsonResult = {
+                'items_page_count': results.length,
+                'page_number': page,
+                'Items': results
+            }
 
-        var myJsonString = JSON.parse(JSON.stringify(jsonResult));
-        res.statusMessage = "Products for page " + page;
-        res.statusCode = 200;
-        res.json(myJsonString);
-        res.end();
-    })
+            var myJsonString = JSON.parse(JSON.stringify(jsonResult));
+            res.statusMessage = "Products for page " + page;
+            res.statusCode = 200;
+            res.json(myJsonString);
+            res.end();
+        })
+    } else {
+        //const page = req.query.page
+        //const offset = (page - 1) * limit
+        const table = req.params.table
+        const prodsQuery = "select * from " + table
+        connection.query(prodsQuery, function (error, results, fields) {
+            if (error) throw error;
+            var jsonResult = {
+                'items_page_count': results.length,
+                'Items': results
+            }
+
+            var myJsonString = JSON.parse(JSON.stringify(jsonResult));
+            res.statusCode = 200;
+            res.json(myJsonString);
+            res.end();
+        })
+    }
+
 }
 
 exports.getAllproducts = (req, res) => {
@@ -68,24 +88,41 @@ exports.getAllproducts = (req, res) => {
 
 exports.getlessPrice = (req, res) => {
     const limit = 25
-    const page = req.query.page
-    const offset = (page - 1) * limit
-    const category = req.params.category
-    const query = "SELECT p.name AS `Product Name`, s.name AS `Provider Name`, c.name, MIN(price) AS `Min Price` FROM product_providers AS ps JOIN products AS p ON ps.products_id = p.id JOIN providers AS s ON ps.providers_id = s.id JOIN category as c on c.id = p.category_id WHERE c.name =" + category + " GROUP BY p.name" + " limit " + limit + " OFFSET " + offset
-    connection.query(query, function (error, results, fields) {
-        if (error) throw error;
-        var jsonResult = {
-            'items_page_count': results.length,
-            'page_number': page,
-            'Items': results
-        }
+    if(req.query.page){
+        const page = req.query.page
+        const offset = (page - 1) * limit
+        const category = req.params.category
+        const query = "SELECT p.name AS `Product Name`, s.name AS `Provider Name`, c.name, MIN(price) AS `Min Price` FROM product_providers AS ps JOIN products AS p ON ps.products_id = p.id JOIN providers AS s ON ps.providers_id = s.id JOIN category as c on c.id = p.category_id WHERE c.name =" + category + " GROUP BY p.name" + " limit " + limit + " OFFSET " + offset
+        connection.query(query, function (error, results, fields) {
+            if (error) throw error;
+            var jsonResult = {
+                'items_page_count': results.length,
+                'page_number': page,
+                'Items': results
+            }
+    
+            var myJsonString = JSON.parse(JSON.stringify(jsonResult));
+            res.statusMessage = "Products for page " + page;
+            res.statusCode = 200;
+            res.json(myJsonString);
+            res.end();
+        })
+    } else {
+        const category = req.params.category
+        const query = "SELECT p.name AS `Product Name`, s.name AS `Provider Name`, c.name, MIN(price) AS `Min Price` FROM product_providers AS ps JOIN products AS p ON ps.products_id = p.id JOIN providers AS s ON ps.providers_id = s.id JOIN category as c on c.id = p.category_id WHERE c.name =" + category + " GROUP BY p.name"
+        connection.query(query, function (error, results, fields) {
+            if (error) throw error;
+            var jsonResult = {
+                'items_page_count': results.length,
+                'Items': results
+            }
+            var myJsonString = JSON.parse(JSON.stringify(jsonResult));
+            res.statusCode = 200;
+            res.json(myJsonString);
+            res.end();
+        })
+    }
 
-        var myJsonString = JSON.parse(JSON.stringify(jsonResult));
-        res.statusMessage = "Products for page " + page;
-        res.statusCode = 200;
-        res.json(myJsonString);
-        res.end();
-    })
 }
 
 exports.getFeatured = (req, res) => {
